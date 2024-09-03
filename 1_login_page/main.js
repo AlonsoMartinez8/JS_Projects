1f49d8bb1c5480d6de88b88d453ad41c688e2d4a
AlonsoMartinez8 <alonsomartinez0804@gmail.com>
2024-09-03 20:28:17 +0200

    MODIFY: Refactorized logic

--- 1_login_page/main.js
+++ 1_login_page/main.js
@@ -1,37 +1,36 @@
 // Region Consts
 const $ = element => document.querySelector(element)
 const $$ = elements => document.querySelectorAll(elements)
-const $passwordInput = $('input[name="password"]')
 const $confirmPasswordInput = $('input[name="confirmPassword"]')
+const $eMailInput = $('input[name="eMail"]')
 const $eyeClosed = $('svg.eyeClosed')
 const $eyeOpen = $('svg.eyeOpen')
-const $eMailInput = $('input[name="eMail"]')
 const $formState = $('.formState')
-const STRINGS = {
-    Valid: 'valid',
-    Invalid: 'invalid',
-    DisplayNone: 'none',
-    DisplayBlock: 'block',
-    TypeText: 'text',
-    TypePassword: 'password',
-    Empty: '',
-    STATES: {
-        InvalidPassword: 'Invalid password',
-        PasswordsDoNotCoincide: 'Passwords do not coincide',
-        InvalidEMail: 'Invalid E-Mail',
-        EmptyFields: 'Fill every field',
-        CompletedAndValid: 'All fields are completed and valid',
-    }
-}
+const $nameInput = $('input[name="name"]')
+const $passwordInput = $('input[name="password"]')
+
 const STATES = {
-    Uncompleted: 'Uncompleted',
-    Invalid:{
-        InvalidPassword: 'InvalidPassword',
-        PasswordsDoNotCoincide: 'PasswordsDoNotCoincide',
-        InvalidEMail: 'InvalidEMail',
-    },
-    Completed: 'Completed',
+    Completed: 1,
+    InvalidCredentials: 0,
+    Uncompleted: -1,
 }
+
+const STRINGS = {
+    DisplayBlock: 'block',
+    DisplayNone: 'none',
+    Empty: '',
+    Invalid: 'invalid',
+    STATES: {
+        CompletedAndValid: 'All fields are completed and valid',
+        EmptyFields: 'Fill every field',
+        InvalidCredentials: 'Invalid credentials. Please check the fields',
+    },
+    TypePassword: 'password',
+    TypeText: 'text',
+    Uncompleted: 'uncompleted',
+    Valid: 'valid',
+}
+
 let _currentState = STATES.Uncompleted
 Object.defineProperty(window, 'currentState', {
     get() {
@@ -45,89 +44,127 @@ Object.defineProperty(window, 'currentState', {
 // End Region Consts
 
 // Region Listeners
-$passwordInput.addEventListener('input', (e, input = $passwordInput) => {
-    if (e.target.value === STRINGS.Empty) {
-        input.classList.remove(STRINGS.Valid)
-        input.classList.remove(STRINGS.Invalid)
-        window.currentState = STATES.Uncompleted
-    } else if (checkPasswordPolicy(e.target.value)) {
-        input.classList.remove(STRINGS.Invalid)
-        input.classList.add(STRINGS.Valid)
-    } else {
-        input.classList.remove(STRINGS.Valid)
-        input.classList.add(STRINGS.Invalid)
-        window.currentState = STATES.Invalid.InvalidPassword
-    }
-    validateFormCompletion()
-})
 $confirmPasswordInput.addEventListener('input', (e, input = $confirmPasswordInput) => {
     if (e.target.value === STRINGS.Empty) {
         input.classList.remove(STRINGS.Valid)
         input.classList.remove(STRINGS.Invalid)
+        input.classList.add(STRINGS.Uncompleted)
         window.currentState = STATES.Uncompleted
     } else if (checkPasswordsCoincide(e.target.value)) {
-        input.classList.remove(STRINGS.Invalid)
-        input.classList.add(STRINGS.Valid)
+        if (!checkPasswordPolicy(e.target.value)) {
+            input.classList.remove(STRINGS.Valid)
+            input.classList.add(STRINGS.Invalid)
+            input.classList.remove(STRINGS.Uncompleted)
+            window.currentState = STATES.InvalidCredentials
+        } else {
+            input.classList.add(STRINGS.Valid)
+            input.classList.remove(STRINGS.Invalid)
+            input.classList.remove(STRINGS.Uncompleted)
+        }
     } else {
         input.classList.remove(STRINGS.Valid)
         input.classList.add(STRINGS.Invalid)
-        window.currentState = STATES.Invalid.PasswordsDoNotCoincide
+        input.classList.remove(STRINGS.Uncompleted)
+        window.currentState = STATES.InvalidCredentials
     }
     validateFormCompletion()
 })
-$eyeClosed.addEventListener('click', () => { setPasswordVisible(true) })
-$eyeOpen.addEventListener('click', () => { setPasswordVisible(false) })
+
 $eMailInput.addEventListener('input', (e, input = $eMailInput) => {
     if (e.target.value === STRINGS.Empty) {
         input.classList.remove(STRINGS.Valid)
         input.classList.remove(STRINGS.Invalid)
+        input.classList.add(STRINGS.Uncompleted)
         window.currentState = STATES.Uncompleted
     } else if (checkEMail(e.target.value)) {
-        input.classList.remove(STRINGS.Invalid)
         input.classList.add(STRINGS.Valid)
+        input.classList.remove(STRINGS.Invalid)
+        input.classList.remove(STRINGS.Uncompleted)
     } else {
         input.classList.remove(STRINGS.Valid)
         input.classList.add(STRINGS.Invalid)
-        window.currentState = STATES.Invalid.InvalidEMail
+        input.classList.remove(STRINGS.Uncompleted)
+        window.currentState = STATES.InvalidCredentials
+    }
+    validateFormCompletion()
+})
+
+$eyeClosed.addEventListener('click', () => { setPasswordVisible(true) })
+$eyeOpen.addEventListener('click', () => { setPasswordVisible(false) })
+
+$nameInput.addEventListener('input', (e, input = $nameInput) => {
+    if (e.target.value === STRINGS.Empty) {
+        input.classList.remove(STRINGS.Valid)
+        input.classList.remove(STRINGS.Invalid)
+        input.classList.add(STRINGS.Uncompleted)
+        window.currentState = STATES.Uncompleted
+    } else {
+        input.classList.add(STRINGS.Valid)
+        input.classList.remove(STRINGS.Invalid)
+        input.classList.remove(STRINGS.Uncompleted)
+    }
+    validateFormCompletion()
+})
+
+$passwordInput.addEventListener('input', (e, input = $passwordInput) => {
+    if (e.target.value === STRINGS.Empty) {
+        input.classList.remove(STRINGS.Valid)
+        input.classList.remove(STRINGS.Invalid)
+        input.classList.add(STRINGS.Uncompleted)
+        window.currentState = STATES.Uncompleted
+    } else if (checkPasswordPolicy(e.target.value)) {
+        input.classList.add(STRINGS.Valid)
+        input.classList.remove(STRINGS.Invalid)
+        input.classList.remove(STRINGS.Uncompleted)
+    } else {
+        input.classList.remove(STRINGS.Valid)
+        input.classList.add(STRINGS.Invalid)
+        input.classList.remove(STRINGS.Uncompleted)
+        window.currentState = STATES.InvalidCredentials
     }
     validateFormCompletion()
 })
 // End Region Listeners
 
 // Region Validation
-const checkPasswordPolicy = (password) => {
-    const hasLowerCase = /[a-z]/.test(password)
-    const hasUpperCase = /[A-Z]/.test(password)
-    const hasNumber = /\d/.test(password)
-    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password)
-    return hasLowerCase && hasUpperCase && hasNumber && hasSymbol
-}
-const checkPasswordsCoincide = (confirmPassword) => {
-    return confirmPassword === $passwordInput.value
-}
 const checkEMail = (email) => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     return emailPattern.test(email)
 }
+
+const checkPasswordPolicy = (password) => {
+    const hasLowerCase = /[a-z]/.test(password)
+    const hasNumber = /\d/.test(password)
+    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+    const hasUpperCase = /[A-Z]/.test(password)
+    return hasLowerCase && hasUpperCase && hasNumber && hasSymbol
+}
+
+const checkPasswordsCoincide = (confirmPassword) => {
+    return confirmPassword === $passwordInput.value
+}
+
 const validateFormCompletion = () => {
+    const isEmailValid = $eMailInput.classList.contains(STRINGS.Valid)
+    const isNameValid = $nameInput.classList.contains(STRINGS.Valid)
     const isPasswordValid = $passwordInput.classList.contains(STRINGS.Valid)
     const isConfirmPasswordValid = $confirmPasswordInput.classList.contains(STRINGS.Valid)
-    const isEmailValid = $eMailInput.classList.contains(STRINGS.Valid)
 
-    if (isPasswordValid && isConfirmPasswordValid && isEmailValid) {
-        window.currentState = STATES.Completed
-    } else if ($passwordInput.value === STRINGS.Empty || 
-               $confirmPasswordInput.value === STRINGS.Empty || 
-               $eMailInput.value === STRINGS.Empty) {
+    if ($passwordInput.value === STRINGS.Empty ||
+        $confirmPasswordInput.value === STRINGS.Empty ||
+        $eMailInput.value === STRINGS.Empty ||
+        $nameInput.value === STRINGS.Empty) {
         window.currentState = STATES.Uncompleted
-    } else {
-        if (!isPasswordValid) {
-            window.currentState = STATES.Invalid.InvalidPassword
-        } else if (!isConfirmPasswordValid) {
-            window.currentState = STATES.Invalid.PasswordsDoNotCoincide
-        } else if (!isEmailValid) {
-            window.currentState = STATES.Invalid.InvalidEMail
-        }
+        return
+    }
+
+    if (!isEmailValid || !isPasswordValid || !isConfirmPasswordValid) {
+        window.currentState = STATES.InvalidCredentials
+        return
+    }
+
+    if (isEmailValid && isNameValid && isPasswordValid && isConfirmPasswordValid) {
+        window.currentState = STATES.Completed
     }
 }
 // End Region Validation
@@ -144,26 +181,27 @@ const setPasswordVisible = (visible) => {
         $eyeOpen.style.display = STRINGS.DisplayNone
     }
 }
+
 const updateFormState = (state) => {
     switch (state) {
-        case STATES.Idle:
-            $formState.textContent = STRINGS.Empty
-            break;
         case STATES.Uncompleted:
             $formState.textContent = STRINGS.STATES.EmptyFields
-            break;
-        case STATES.Invalid.InvalidPassword:
-            $formState.textContent = STRINGS.STATES.InvalidPassword
-            break;
-        case STATES.Invalid.PasswordsDoNotCoincide:
-            $formState.textContent = STRINGS.STATES.PasswordsDoNotCoincide
-            break;
-        case STATES.Invalid.InvalidEMail:
-            $formState.textContent = STRINGS.STATES.InvalidEMail
-            break;
+            $formState.classList.remove(STRINGS.Valid)
+            $formState.classList.remove(STRINGS.Invalid)
+            $formState.classList.add(STRINGS.Uncompleted)
+            break
+        case STATES.InvalidCredentials:
+            $formState.textContent = STRINGS.STATES.InvalidCredentials
+            $formState.classList.remove(STRINGS.Valid)
+            $formState.classList.remove(STRINGS.Uncompleted)
+            $formState.classList.add(STRINGS.Invalid)
+            break
         case STATES.Completed:
             $formState.textContent = STRINGS.STATES.CompletedAndValid
-            break;
+            $formState.classList.remove(STRINGS.Invalid)
+            $formState.classList.remove(STRINGS.Uncompleted)
+            $formState.classList.add(STRINGS.Valid)
+            break
         default:
             $formState.textContent = STRINGS.Empty
     }
